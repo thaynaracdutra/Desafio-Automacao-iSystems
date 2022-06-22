f470ccb6e928d921631d92fba77af03b1048c564
xuxu <thaynaracdutra@outlook.com>
2022-06-22 12:07:22 -0300

    desafio automacao iSystems

--- cypress/e2e/automacao.cy.js
+++ cypress/e2e/automacao.cy.js
@@ -3,34 +3,36 @@ describe("automacao iSystems", () => {
     cy.visit("https://www.saucedemo.com/");
   });
 
+  it("Login foi realizado", () => {
+    cy.login("standard_user", "secret_sauce");
 
-it("Login foi realizado", () => {
-  cy.login('standard_user', 'secret_sauce')
-  
-  cy.location('pathname').should('eq', '/inventory.html')
-  cy.contains('.title', 'Products').should("be.visible");
-});
+    cy.location("pathname").should("eq", "/inventory.html");
+    cy.contains(".title", "Products").should("be.visible");
+  });
 
-it("Produto deve estar incluso no carrinho", () => {
-  cy.login('standard_user', 'secret_sauce')
-  cy.get('[data-test="add-to-cart-test.allthethings()-t-shirt-(red)"]').click()
+  it("Produto deve estar incluso no carrinho", () => {
+    cy.login("standard_user", "secret_sauce");
+    cy.get(
+      '[data-test="add-to-cart-test.allthethings()-t-shirt-(red)"]'
+    ).click();
 
-  cy.contains('.shopping_cart_badge', 1).should("be.visible");
+    cy.contains(".shopping_cart_badge", 1).should("be.visible");
 
-  cy.get('.shopping_cart_link').click()
+    cy.get(".shopping_cart_link").click();
 
-  cy.location('pathname').should('eq', '/cart.html')
-  cy.contains('.inventory_item_name', 'Test.allTheThings() T-Shirt (Red)').should("be.visible")
-});
+    cy.location("pathname").should("eq", "/cart.html");
+    cy.contains(
+      ".inventory_item_name",
+      "Test.allTheThings() T-Shirt (Red)"
+    ).should("be.visible");
+  });
 
+  it("Logout foi realizado", () => {
+    cy.login("standard_user", "secret_sauce");
+    cy.get("#react-burger-menu-btn").click();
+    cy.get("#logout_sidebar_link").click();
 
-it("Logout foi realizado", () => {
-  cy.login('standard_user', 'secret_sauce')
-  cy.get('#react-burger-menu-btn').click()
-  cy.get('#logout_sidebar_link').click()
-
-  cy.contains('[data-test="login-button"]', 'Login').should("be.visible")
-  cy.location('pathname').should('eq', '/')
-});
-
+    cy.contains('[data-test="login-button"]', "Login").should("be.visible");
+    cy.location("pathname").should("eq", "/");
+  });
 });
